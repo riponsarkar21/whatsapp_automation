f3ef65dc0406c912f9f80671157ce4fed20efe10
riponsarkar21 <141989340+riponsarkar21@users.noreply.github.com>
2025-08-20 05:31:33 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 # config.py
 # Twilio credentials (keep this file private, do NOT push to GitHub)
 
-TWILIO_ACCOUNT_SID = 'AC46c8025107d3bd22a8edd8a6ab317029'
-TWILIO_AUTH_TOKEN = 'eae6ffcef985f7089fa0b25d9b0b4104'
+TWILIO_ACCOUNT_SID = ''
+TWILIO_AUTH_TOKEN = ''
 TWILIO_WHATSAPP_NUMBER = 'whatsapp:+14155238886'  # Sandbox or approved WhatsApp number
 
 # Recipient number(s)
 RECIPIENT_NUMBER = '+8801928078420'   # You can add more numbers later
 # RECIPIENT_NUMBER_2 = '+8801766695428'
+
